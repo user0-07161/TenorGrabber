339ce8bb72ad08f7dab3148c0da43655bd7bb225
User0 <104380885+user0-tb@users.noreply.github.com>
2024-05-29 09:43:28 +0200

    Update tenorgrabber.py

--- TenorGrabber/tenorgrabber.py
+++ TenorGrabber/tenorgrabber.py
@@ -3,7 +3,8 @@ import re
 def getgiflink(url):
     if re.findall(r'https?://tenor.com/view\S+', url):
       tenor_http = requests.get(url)
-      direct_link = re.findall(r"https?://media1.tenor.com/m\S+.gif", tenor_http.text)
+      direct_link_list = re.findall(r"https?://media1.tenor.com/m\S+.gif", tenor_http.text)
+      direct_link = direct_link_list[0]
       return direct_link
     else:
       print("Link is not a tenor link.")
