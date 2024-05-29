0e2bc3aaf27e4a71bdcd13a0ac50daeab47c16be
User0 <104380885+user0-tb@users.noreply.github.com>
2024-05-29 10:05:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name='TenorGrabber',
     packages=find_packages(),
-    version='0.9.1',
+    version='1.0.0',
     description='Tired of /view links on tenor? This python library allows you to get the direct link of the GIF!',
     author='user0',
     long_description=long_description,
