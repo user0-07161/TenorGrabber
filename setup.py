baf7b5a86ec2b35e47ffb5fa8e27a9fad35519c1
User0 <104380885+user0-tb@users.noreply.github.com>
2024-05-29 10:41:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import find_packages, setup
 from pathlib import Path
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 setup(
@@ -10,7 +8,9 @@ setup(
     version='1.0.1',
     description='Tired of /view links on tenor? This python library allows you to get the direct link of the GIF!',
     author='user0',
-    install_reqs = requirements,
+    install_reqs=[
+    'requests'
+    ],
     long_description=long_description,
     long_description_content_type='text/markdown',
 )
