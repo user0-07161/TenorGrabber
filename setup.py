ac3b7f262c1a7e6f4820c3a0eeb1d9581e49b3e2
User0 <104380885+user0-07161@users.noreply.github.com>
2024-05-30 13:00:56 +0200

    Small update to the metadata

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name='TenorGrabber',
     packages=find_packages(),
-    version='1.0.4',
+    version='1.0.5',
     author='user0',
     description='Get the direct link to a tenor GIF on python3!',
     install_requires=[
     'requests'
     ],
+    url='https://github.com/user0-07161/TenorGrabber/',
     long_description=long_description,
     long_description_content_type='text/markdown',
 )
